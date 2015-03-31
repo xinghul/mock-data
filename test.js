f566034619eb0fc5c0cb50fb03266d0ab0290be7
Levi Lu <llu@xcalar.com>
2015-03-30 17:33:55 -0700

    reconstruct finished

--- test.js
+++ test.js
@@ -1,13 +1,33 @@
 +function (undefined) {
     "use strict";
 
-    var rdate = require("./lib/date");
+    var rInt = require("./lib/integer");
 
-    rdate.setParams({format: "MMMM Do YYYY, h:mm:ss a"});
+    var rInt1 = rInt(0, 123);
 
+    // console.log(rInt1._start);
 
-    console.dir(rdate);
+    console.log(rInt1.getParams());
 
+    // rInt1.setParams({start: 0});
 
+    // console.log(rInt1.getParams());
+
+    var rInt2 = rInt();
+
+    console.log(rInt2.getParams());
+    console.log(rInt1.getParams());
+
+    rInt1.setParams({end: 100});
+    console.log(rInt2.getParams());
+    console.log(rInt1.getParams());
+
+    // var rDate1 = require("./lib/date")();
+    // console.log(rDate1.getParams());
+    // rDate1.setParams({start: 0});
+    // console.log(rDate1.getParams());
+    // var rDate2 = require("./lib/date")();
+    // console.log(rDate2.getParams());
+    // console.log(rDate1.getParams());
 
 }();
--- test/date.js
+++ test/date.js
@@ -4,28 +4,33 @@
     var should = require("should")
     ,   moment = require("moment");
 
-    var rdate  = require("../lib/date")
-    ,   format = "MMMM Do YYYY, h:mm:ss a";
+    var rDate  = require("../lib/date");
+
+    var rDate1
+    ,   rDate2;
 
     describe("Test date", function() {
         describe("Test basics", function() {
-            it("should exist", function (done) {
-                rdate.should.exist;
+            before(function() {
+                rDate1 = rDate();
+            });
+            it("should exist and is a function", function (done) {
+                rDate.should.exist && rDate.should.be.a.Function;
 
                 done();
             });
-            it("should not be able to get parameters directly", function (done) {
-                should.not.exist(rdate.start);
-                should.not.exist(rdate.end);
-                should.not.exist(rdate.isUTC);
-                should.not.exist(rdate.format);
+            it("should be able to get internal parameters directly", function (done) {
+                should.exist(rDate1._start);
+                should.exist(rDate1._end);
+                should.exist(rDate1._isUTC);
+                should(rDate1._format).be.null;
 
                 done();
             });
             it("should have default params and be able to get params by getParams()", function (done) {
-                rdate.getParams.should.exist && rdate.getParams.should.be.a.Function;
+                rDate1.getParams.should.exist && rDate1.getParams.should.be.a.Function;
 
-                var params = rdate.getParams();
+                var params = rDate1.getParams();
 
                 params.should.be.an.Object;
                 should(params.start).be.a.Number;
@@ -36,38 +41,62 @@
                 done();
             });
             it("should be able to set parameters by setParams()", function (done) {
-                rdate.setParams.should.exist && rdate.setParams.should.be.a.Function;
+                rDate1.setParams.should.exist && rDate1.setParams.should.be.a.Function;
 
                 var newParams = {
                     start  : 1989,
                     end    : 2012,
                     isUTC  : true,
-                    format : format
+                    format : "MMMM Do YYYY, h:mm:ss a"
                 };
 
-                rdate.setParams(newParams);
+                rDate1.setParams(newParams);
 
-                var afterSetParams = rdate.getParams();
+                var afterSetParams = rDate1.getParams();
 
                 afterSetParams.should.be.an.Object;
-                should(afterSetParams.start).equal(1989);
-                should(afterSetParams.end).equal(2012);
-                should(afterSetParams.isUTC).equal(true);
-                should(afterSetParams.format).equal("MMMM Do YYYY, h:mm:ss a");
+                should(afterSetParams.start).equal(newParams.start);
+                should(afterSetParams.end).equal(newParams.end);
+                should(afterSetParams.isUTC).equal(newParams.isUTC);
+                should(afterSetParams.format).equal(newParams.format);
+
+                done();
+            });
+            it("should generate a new object each time calling rDate()", function (done) {
+                rDate1 = rDate();
+                rDate1.setParams({
+                    start  : 1989,
+                    end    : 2012,
+                    isUTC  : true,
+                    format : "MMMM Do YYYY, h:mm:ss a"
+                });
+
+                rDate2 = rDate();
+                rDate2.setParams({
+                    start  : 1990,
+                    end    : 2002,
+                    isUTC  : false,
+                    format : null
+                });
+
+                should(rDate1._start).not.equal(rDate2._start);
+                should(rDate1._end).not.equal(rDate2._end);
+                should(rDate1._isUTC).not.equal(rDate2._isUTC);
+                should(rDate1._format).not.equal(rDate2._format);
 
                 done();
             });
         });
 
         describe("Test correctness", function() {
+            before(function() {
+                rDate2 = rDate();
+            });
             it("should be able to generate valid date using generate()", function (done) {
-                rdate.generate.should.exist && rdate.generate.should.be.a.Function;
+                rDate2.generate.should.exist && rDate2.generate.should.be.a.Function;
 
                 for (var i = 0; i < 100; i ++) {
-                    var date = rdate.generate();
-                    if (!moment(format, rdate.generate()).isValid())
-                        console.log(date);
-                    // should(moment(format, rdate.generate()).isValid()).be.true;
+                    should(moment(rDate2.generate()).isValid()).be.true;
                 }
                 
                 done();
@@ -75,18 +104,17 @@
             it("should generate year in range", function (done) {
                 var start = 1989
                 ,   end   = 1992;
-                rdate.setParams({
+                rDate2.setParams({
                     start : start,
                     end   : end,
                     format: "YYYY"
                 });
 
                 for (var i = 0, y = 0; i < 100; i ++) {
-                    y = parseInt(rdate.generate());
+                    y = parseInt(rDate1.generate());
                     should(y).not.greaterThan(end).and.not.lessThan(start);
                 }
 
-                
                 done();
             });
         });
